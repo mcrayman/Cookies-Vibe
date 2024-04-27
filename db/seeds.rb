98d48d35a78aed706b489783de0a6158aa29ac52
Matt McMeans <mrmcmans@memphis.edu>
2024-04-27 11:55:36 -0500

    posts controller and first seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+first_day = Post.create!(
+  title: "Cookie's first day home", 
+  description: 'The first day I brought home Cookie', 
+  mood: 'Happy', 
+  date: Date.parse('2024-05-14'), 
+  age: 1)
+
+first_day.image.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'first_day.jpeg')), filename: 'first_day.jpeg', content_type: 'image/jpeg')
