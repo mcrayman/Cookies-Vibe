ca4faa94508cce2a435485a9babc0d566c5c556b
Matt McMeans <mrmcmans@memphis.edu>
2024-04-27 12:31:51 -0500

    posts seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,39 @@ first_day = Post.create!(
   age: 1)
 
 first_day.image.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'first_day.jpeg')), filename: 'first_day.jpeg', content_type: 'image/jpeg')
+
+first_attack = Post.create!(
+  title: "Cookie's first time attacking me", 
+  description: 'She really was just going for a hug', 
+  mood: 'Angry', 
+  date: Date.parse('2024-05-15'), 
+  age: 1)
+
+first_attack.image.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'first_attack.jpeg')), filename: 'first_attack.jpeg', content_type: 'image/jpeg')
+
+bean_bag = Post.create!(
+  title: "New Bed", 
+  description: 'Cookie decided she would sleep on the bean bag instead of her bed', 
+  mood: 'Sleepy', 
+  date: Date.parse('2024-10-14'), 
+  age: 1)
+
+bean_bag.image.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'bean_bag.jpeg')), filename: 'bean_bag.jpeg', content_type: 'image/jpeg')
+
+boyfriend = Post.create!(
+  title: "Cookie meets her boyfriend", 
+  description: 'Cookie would always watch the neighbors cat throught the window, but one day he came to visit', 
+  mood: 'Happy', 
+  date: Date.parse('2024-10-17'), 
+  age: 1)
+
+boyfriend.image.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'boyfriend.jpeg')), filename: 'boyfriend.jpeg', content_type: 'image/jpeg')
+
+bat = Post.create!(
+  title: "BAT CAT!", 
+  description: 'For halloween I dressed Cookie up as a bat, she was not happy about it.', 
+  mood: 'Angry', 
+  date: Date.parse('2024-10-30'), 
+  age: 1)
+
+bat.image.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'bat.jpeg')), filename: 'bat.jpeg', content_type: 'image/jpeg')
